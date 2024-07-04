719ac2ca07800a3860498f0d2a60b896f40adde1
Ignacio Hardoy <hartoyy@hotmail.com>
2024-07-04 12:46:56 -0300

    fix module marque

--- src/components/marquee/ScrollingText.js
+++ src/components/marquee/ScrollingText.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Marquee from 'react-fast-marquee'
-import styles from './mARQUE.module.css'
+import styles from './Marque.module.css'
 
 const ScrollingText = () => {
   return (
